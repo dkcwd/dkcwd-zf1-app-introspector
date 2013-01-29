0be808b71db19057b1832099699de96f780a37c8
Dave Clark <dave@dkcwd.com.au>
2013-01-29 16:54:43 +1100

    Update Dkcwd/AppIntrospector/AppIntrospector.php
    Added an extra space for readability

--- Dkcwd/AppIntrospector/AppIntrospector.php
+++ Dkcwd/AppIntrospector/AppIntrospector.php
@@ -2,6 +2,7 @@
 /** 
  * A class for use with Zend Framework (v1) applications. 
  * Provides a way to quickly generate an array representing the structure of your application.
+ * 
  * @link http://github.com/dkcwd/dkcwd-zf1-app-introspector for the canonical source repository
  * @author Dave Clark dave@dkcwd.com.au 
  * @copyright Dave Clark 2013
